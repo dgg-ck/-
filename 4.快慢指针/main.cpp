d488e032bd71defef53e2c18bf8723c5247ab8dd
dgg <m15102032890@163.com>
2026-06-24 11:31:08 +0800

    快慢指针和循环双链表的恢复

--- 4.快慢指针/main.cpp
+++ 4.快慢指针/main.cpp
@@ -21,11 +21,23 @@ int main()
 	HeadInsert(*pList, *(new Node(8)));
 	HeadInsert(*pList, *(new Node(1)));*/
 	//HeadInsert(*pList, *(new Node(2)));
-	//HeadInsert(*pList, *(new Node(3)));	
+	//HeadInsert(*pList, *(new Node(3)));
+
+	//故障环代码
+	//pList->pHead->pNext->pNext->pNext->pNext->pNext = pList->pHead->pNext->pNext;
 
 	//SearchNode(*pList, 10);
 	
 
+	//判断链表中有没有环
+	if (IsLoop(*pList))
+	{
+		std::cout << "链表中有故障环" << std::endl;
+	}
+	else
+	{
+		std::cout << "链表中有无故障环" << std::endl;
+	}
 
 
 	//删除结点
